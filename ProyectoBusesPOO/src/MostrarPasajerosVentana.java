1c8a9f5e4e718a387b253e495c0dfe38a6b207bd
Sorlak12 <88897677+Sorlak12@users.noreply.github.com>
2021-11-15 15:12:03 -0300

    Add files via upload

--- ProyectoBusesPOO/src/MostrarPasajerosVentana.java
+++ ProyectoBusesPOO/src/MostrarPasajerosVentana.java
@@ -7,6 +7,7 @@ public class MostrarPasajerosVentana extends javax.swing.JFrame {
     private JButton atrasButton;
     private JTextPane ListaPasajerosText;
     private JPanel MostrarPasajerosFrame;
+    private JScrollPane ScrollPane;
 
     public MostrarPasajerosVentana(String title, AgenciaBuses Gerencia){
         super(title);
@@ -14,6 +15,7 @@ public class MostrarPasajerosVentana extends javax.swing.JFrame {
         this.setContentPane(MostrarPasajerosFrame);
         this.setSize(600, 500);
         ListaPasajerosText.setText(Gerencia.mostrarPasajeros());
+        ListaPasajerosText.setCaretPosition(0);
         atrasButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
