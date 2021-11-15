55424cec317c8bf5636dbf52d636eff3db565e60
Sorlak12 <88897677+Sorlak12@users.noreply.github.com>
2021-11-15 15:07:34 -0300

    Add files via upload

--- ProyectoBusesPOO/src/VentanaMostrarBuses.java
+++ ProyectoBusesPOO/src/VentanaMostrarBuses.java
@@ -4,22 +4,21 @@ import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 public class VentanaMostrarBuses extends javax.swing.JFrame {
     private JButton atrasButton;
     private JTextArea ListasBusesTexto;
     private JPanel MostrarBusesPanel;
-    private JScrollBar scrollBar1;
+    private JScrollPane ScrollPane;
 
     public VentanaMostrarBuses(String title, AgenciaBuses Gerenecia){
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(MostrarBusesPanel);
         this.setSize(600, 800);
-        scrollBar1.setMaximum(800);
-        scrollBar1.setMinimum(1);
-
         ListasBusesTexto.setText(Gerenecia.mostrarBuses());
+        ListasBusesTexto.setCaretPosition(0);
         atrasButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -34,12 +33,7 @@ public class VentanaMostrarBuses extends javax.swing.JFrame {
                 dispose();
             }
         });
-        scrollBar1.addAdjustmentListener(new AdjustmentListener() {
-            @Override
-            public void adjustmentValueChanged(AdjustmentEvent e) {
-                
-            }
-        });
+
     }
 
 }
